0343a49e23c2871c2eb848848ece938a8ad12a48
michaelweiser <michaelweiser@0511fff6-642a-0410-b4b1-cbf9baaaddce>
2012-01-31 19:05:06 +0000

    support bereitschaftsfile parameter in mac wrapper script
    git-svn-id: https://sctime.svn.sourceforge.net/svnroot/sctime/trunk/sctime@306 0511fff6-642a-0410-b4b1-cbf9baaaddce

--- extra/mac/sctime.sh
+++ extra/mac/sctime.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-if [ -f "$HOME/.sctime/zeitkonten" ] ; then
-	exec -a $0 $0.bin --zeitkontenfile=$HOME/.sctime/zeitkonten
-fi
+[ -n "$SCTIME_DIR" ] || SCTIME_DIR="$HOME"/.sctime
+[ -n "$SCTIME_ZEITKONTENFILE" ] || SCTIME_ZEITKONTENFILE="$SCTIME_DIR"/zeitkonten
+[ -n "$SCTIME_BEREITSCHAFTSFILE" ] || SCTIME_BEREITSCHAFTSFILE="$SCTIME_DIR"/bereitschaft
 
-exec -a $0 $0.bin
+args=""
+[ -f "$SCTIME_ZEITKONTENFILE" ] && args="$args --zeitkontenfile=\"\$SCTIME_ZEITKONTENFILE\""
+[ -f "$SCTIME_BEREITSCHAFTSFILE" ] && args="$args --bereitschaftsfile=\"\$SCTIME_BEREITSCHAFTSFILE\""
+
+eval exec -a $0 $0.bin $args
